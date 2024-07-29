a5e48fa3f5589f9ae741a7492865ccdbbcedf810
sina jahan <sinajahan418@example.com>
2024-07-29 02:20:20 -0700

    completion of the backend

--- backend/db/connectToMongoDB.js
+++ backend/db/connectToMongoDB.js
@@ -1,12 +1,16 @@
 import mongoose from "mongoose";
 
 const connectToMongoDB = async () => {
-	try {
-		await mongoose.connect(process.env.MONGO_DB_URI);
-		console.log("Connected to MongoDB");
-	} catch (error) {
-		console.log("Error connecting to MongoDB", error.message);
-	}
+  try {
+    if (mongoose.connections[0].readyState) {
+      return true;
+    } else {
+      await mongoose.connect(process.env.MONGO_DB_URI);
+      console.log("Connect To DB Successfully :))");
+    }
+  } catch (err) {
+    console.log("DB Connection has error ->", err);
+  }
 };
 
 export default connectToMongoDB;
--- backend/server.js
+++ backend/server.js
@@ -26,10 +26,10 @@ app.use("/api/users", userRoutes);
 app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
 app.get("*", (req, res) => {
-	res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
 });
 
 server.listen(PORT, () => {
-	connectToMongoDB();
-	console.log(`Server Running on port ${PORT}`);
+  connectToMongoDB();
+  console.log(`Server Running on port ${PORT}`);
 });
